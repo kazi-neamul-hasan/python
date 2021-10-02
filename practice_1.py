6145d91c2b02765528c61cdada6eeb1df4576e96
unknown <champion.rimon@gmail.com>
2021-10-02 21:20:39 +0600

    Test

---
+++ practice_1.py
@@ -0,0 +1,4 @@
+x = "2"
+type(x)
+y = int(x)
+type(y)
--- program.py
+++ program.py
@@ -1,6 +1,11 @@
 #input
 #variables
+from typing import Type
+from typing_extensions import runtime
+
+
 name = input("Enter your name:")
+
 
 #output
 print("Greetings!", name)
